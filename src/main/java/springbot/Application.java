0bd963e2261103ca1151366257a76eb703b96008
PedroIAmaral <pedrobrandao.amaral@gmail.com>
2020-12-16 00:31:39 -0400

    Início do projeto

---
+++ src/main/java/springbot/Application.java
@@ -0,0 +1,33 @@
+package springbot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String args[]){
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+        return args -> {
+
+            System.out.println("Verificando Beans");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String name:
+                 beanNames) {
+                System.out.println(name);
+            }
+        };
+    }
+
+}
---
+++ src/main/java/springbot/HelloController.java
@@ -0,0 +1,14 @@
+package springbot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Oi mundo eu estou criando minha aplicação";
+    }
+
+}
